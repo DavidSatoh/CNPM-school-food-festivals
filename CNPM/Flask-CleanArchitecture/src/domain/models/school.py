4884e61835b61f7ba1a7a50fda533274fa19e04e
Huynh Tan Du <huynhtandu106@gmail.com>
2025-08-25 21:03:35 +0700

    save work from master

--- CNPM/Flask-CleanArchitecture/src/domain/models/school.py
+++
@@ -1,8 +0,0 @@
-class School:
-    def __init__(self, id: int, name: str, address: str):
-        self.id = id
-        self.name = name
-        self.address = address
-
-    def __repr__(self):
-        return f"<School {self.name}>"
