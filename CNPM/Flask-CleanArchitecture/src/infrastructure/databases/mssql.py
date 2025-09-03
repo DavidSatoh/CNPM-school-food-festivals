390503970c4f57a253d024e560f492f7d1364431
Huynh Tan Du <huynhtandu106@gmail.com>
2025-09-04 00:44:54 +0700

    update db

--- CNPM/Flask-CleanArchitecture/src/infrastructure/databases/mssql.py
+++ CNPM/Flask-CleanArchitecture/src/infrastructure/databases/mssql.py
@@ -5,7 +5,10 @@ from infrastructure.databases.base import Base
 
 # Database configuration
 DATABASE_URI = Config.DATABASE_URI
-engine = create_engine(DATABASE_URI)
+engine = create_engine(
+    "mssql+pyodbc://sa:Aa123456@127.0.0.1,1433/FlaskApiDB"
+    "?driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes"
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 session = SessionLocal()
 def init_mssql(app):
